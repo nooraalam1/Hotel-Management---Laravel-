f7439c517478d50bbccd5f4f235c23fd0e34d41e
nooraalam1 <noorsomrat2000@gmail.com>
2026-04-11 16:14:56 +0600

    color changed

--- resources/views/admin/locations/add.blade.php
+++ resources/views/admin/locations/add.blade.php
@@ -9,34 +9,34 @@
                 <a href="{{ route('admin.viewlocations') }}" class="btn btn-info">View Location</a>
             </div>
         </div>
-        <section class="no-padding-top no-padding-bottom">
-            <form action="{{ route('admin.createlocation') }}" method="POST">
+        <section class="no-padding-top no-padding-bottom ">
+            <form action="{{ route('admin.createlocation') }}" method="POST" class="">
                 @csrf
                 <div class="">
-                    <div class="col-3 p-0">
-                        <label>Location</label>
-                        <input type="text" name="location" class="form-control" required>
-                    </div>
-                    <div class="col-3 p-0">
+                    <div class="col-6 p-0">
                         <label>District</label>
-                        <select name="district" id="district" class="form-control" required>
+                        <select name="district" id="district" class="form-control" required style="background-color: #2d3035">
                             <option value="">Select</option>
                             @foreach ($districts as $district)
                                 <option  value="{{ $district->id }}">{{ $district->district_name }}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="col-3 p-0">
+                    <div class="col-6 p-0">
                         <label class="">Division</label>
-                        <input type="text" name="division" class="form-control" id="division" readonly>
+                        <input type="text" name="division" class="form-control" id="division" readonly placeholder="Auto Filled">
                     </div>
-                    <div class="col-3 p-0">
+                    <div class="col-6 p-0 ">
+                        <label>Location</label>
+                        <input type="text" name="location" class="form-control" required placeholder="Road:07, Block:J, Baridhara, Dhaka">
+                    </div>
+                    <div class="col-6 p-0">
                         <label>Location Manager Mobile</label>
-                        <input type="phone" name="phone" class="form-control" required>
+                        <input type="phone" name="phone" class="form-control" required placeholder="+88 017XXXXXXXX">
                     </div>
-                    <div class="col-3 p-0">
+                    <div class="col-6 p-0">
                         <label>Location Manager Email</label>
-                        <input type="email" name="email" class="form-control" required>
+                        <input type="email" name="email" class="form-control" required placeholder="example@mail.com">
                     </div>
                 </div>
                 <div class="d-flex justify-content-center align-items-center">
