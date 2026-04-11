00b067e2c69fb0c6e9fc1b755120df5c99d47f7e
nooraalam1 <noorsomrat2000@gmail.com>
2026-04-11 15:05:18 +0600

    color changed

--- resources/views/admin/locations/add.blade.php
+++ resources/views/admin/locations/add.blade.php
@@ -12,47 +12,6 @@
         <section class="no-padding-top no-padding-bottom">
             <form action="{{ route('admin.createlocation') }}" method="POST">
                 @csrf
-                {{-- <div class="row">
-
-                    <!-- Location -->
-                    <div class="col-md-3 mb-3">
-                        <label>Location</label>
-                        <input type="text" name="location" class="form-control" required>
-                    </div>
-
-                    <!-- District -->
-                    <div class="col-md-3 mb-3">
-                        <label>District</label>
-                        <select name="district_id" id="district" class="form-control" required>
-                            <option value="">Select District</option>
-                            @foreach ($districts as $district)
-                                <option value="{{ $district->id }}" data-division="{{ $district->division_id }}">
-                                    {{ $district->district_name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <!-- Division -->
-                    <div class="col-md-3 mb-3">
-                        <label>Division</label>
-                        <input type="text" id="division" class="form-control" readonly>
-                        <input type="hidden" name="division_id" id="division_id">
-                    </div>
-
-                    <!-- Phone -->
-                    <div class="col-md-3 mb-3">
-                        <label>Location Manager Mobile</label>
-                        <input type="text" name="phone" class="form-control" required>
-                    </div>
-
-                    <!-- Email -->
-                    <div class="col-md-3 mb-3">
-                        <label>Location Manager Email</label>
-                        <input type="email" name="email" class="form-control" required>
-                    </div>
-
-                </div> --}}
                 <div class="">
                     <div class="col-3 p-0">
                         <label>Location</label>
